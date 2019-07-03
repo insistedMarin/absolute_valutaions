0d57e1c0a5c32cca3c3742ab54006431c84f442d
89589 <xyyr190304gmail.com>
2019-07-03 20:15:34 +0800

    新版本二

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+//import 'computation.dart';
+import 'package:managementdemo/Pages/bottombar_page.dart';
+import 'package:managementdemo/Pages/STloan_page.dart';
+import 'package:managementdemo/Pages/LTloan_page.dart';
+import 'package:managementdemo/Pages/login_page.dart';
+
+//import 'dart:async';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(title: 'Value'),
+      routes: {'STloan__page': (context) => STLoan(),
+                'LTloan_page':(context)=>LTLoan(),
+                'login_page':(context)=>Login(),
+                'bottombar_page':(context)=>MyHomePage()},
+    );
+  }
+}
